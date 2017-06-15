cd0a0380955a75eda721a40c5220b50dc2ffbd6f
apeironuranium <lera.leroi@gmail.com>
2017-06-15 19:53:56 +0300

    Add files via upload

---
+++ proxy/src/proxy/Designer.java
@@ -0,0 +1,19 @@
+package proxy;
+
+/**
+ * Created by leral on 15.06.2017.
+ */
+
+
+public class Designer {
+    public void design(){
+        System.out.println("i'm trying");}
+
+    public static void main(String[] args){
+        Designer d = new Ilyabirman();
+        d.design();
+    }
+}
+
+
+
---
+++ proxy/src/proxy/Ilyabirman.java
@@ -0,0 +1,13 @@
+package proxy;
+
+/**
+ * Created by leral on 15.06.2017.
+ */
+public class Ilyabirman extends Designer{
+    @Override
+    public void design() {
+        System.out.println("Redesignall");
+        super.design();
+    }
+
+}
